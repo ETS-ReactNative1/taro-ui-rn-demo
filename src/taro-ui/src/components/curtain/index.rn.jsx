bf749108eea3cc4d0a753e7b14b9319a66d59b1f
lzp <25803471@qq.com>
2021-11-20 23:14:47 +0800

    完善交互

--- src/taro-ui/src/components/curtain/index.rn.jsx
+++ src/taro-ui/src/components/curtain/index.rn.jsx
@@ -1,67 +1,43 @@
 import classNames from 'classnames'
-import {useEffect, useState} from 'react'
 import { View } from '@tarojs/components'
-import RootSiblings from 'react-native-root-siblings';
+import Modal from "react-native-modal";
 import noop from 'lodash/noop';
 import IconFont from '../icon-font';
 import '../../style/components/curtain.scss';
 
-function AtCurtain({ customStyle, className, isOpened = false, closeBtnPosition = 'bottom', onClose = noop, children }) {
-  const [sibling, setSibling] = useState(null);
+function AtCurtain({ isOpened = false, closeBtnPosition = 'bottom', onClose = noop, children }) {
+  const btnCloseClass = classNames({
+    'at-curtain__btn-close': true,
+    'at-curtain__btn-close--image': true,
+    [`at-curtain__btn-close--${closeBtnPosition}`]: closeBtnPosition
+  });
 
-  useEffect(() => {
-    sibling && sibling.destroy();
-    console.log(isOpened);
-    if (isOpened) {
-      if (!sibling) {
-        const curtainClass = classNames(
-          {
-            'at-curtain': true,
-            'at-curtain--closed': !isOpened
-          },
-          className
-        )
-        const btnCloseClass = classNames({
-          'at-curtain__btn-close': true,
-          'at-curtain__btn-close--image': true,
-          [`at-curtain__btn-close--${closeBtnPosition}`]: closeBtnPosition
-        });
-
-        setSibling(new RootSiblings(
+  return (
+    <Modal
+      animationIn='pulse'
+      animationOut='fadeOut'
+      isVisible={isOpened}
+      hasBackdrop
+      onModalHide={onClose}
+      onBackButtonPress={onClose}
+      onBackdropPress={onClose}
+    >
+      <View className='at-curtain__container'>
+        <View className='at-curtain__body'>
+          {children}
           <View
-            className={curtainClass}
-            style={customStyle}
+            className={btnCloseClass}
             onClick={(e) => {
               e && e.stopPropagation && e.stopPropagation()
+              onClose(e)
             }}
           >
-            <View className='at-curtain__container'>
-              <View className='at-curtain__body'>
-                {children}
-                <View
-                  className={btnCloseClass}
-                  onClick={(e) => {
-                    e && e.stopPropagation && e.stopPropagation()
-                    onClose(e)
-                  }}
-                >
-                  <IconFont name='close-circle' size={48} color='#fff' />
-                </View>
-              </View>
-            </View>
+            <IconFont name='close-circle' size={48} color='#fff' />
           </View>
-        ));
-      }
-    } else {
-      sibling && setSibling(null);
-    }
-
-    return () => {
-      sibling && sibling.destroy();
-    };
-  }, [children, className, closeBtnPosition, customStyle, isOpened, onClose]);
-
-  return null;
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 export default AtCurtain;
